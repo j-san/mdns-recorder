8f1184f0d675101245d1607d1e8842d87a1bbe8f
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2014-11-03 22:53:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+mDNS Recorder
+=============
+
+Dynamic Zero Config DNS Server with mDNS.
+
+Listen to local area machine, bind DNS name server and a web view. It allow you to bind names dinamicly for outsider of the subnetwork.
+
+
+Howto
+-----
+
+- Install Avahi on servers in your lan (Mac just works, do nothing)
+- Run this software inside your lan (see "Delpoy" section)
+- Create a DNS record to point on mDNS Recorder
+- Just use the web view
+
+
+Local
+-----
+
+```
+git clone https://github.com/j-san/mdns-recorder.git
+cd mdns-recorder
+npm install
+
+npm start
+# you should open http://localhost:300/
+# or query DNS
+dig @localhost foo.subnet.lan  ANY -p 3053
+
+npm test
+```
+
+Deploy
+------
+
+```
+npm install mdns-recorder
+gem install foreman
+edit mdns-recorder-env
+
+sudo foreman export upstart /etc/init/ \
+    --app=mdns-recorder \
+    --procfile= node_modules/mdns-recorder/Procfile \
+    --env=mdns-recorder-env
+
+sudo start mdns-recorder
+```
---
+++ index.js
@@ -0,0 +1,156 @@
+
+var dns = require('native-dns'),
+    mdns = require('mdns'),
+    util = require('util'),
+    express = require('express');
+
+var MDNS_DOMAIN = process.env.MDNS_DOMAIN || '.local.';
+var BIND_DNS_DOMAIN = process.env.BIND_DNS_DOMAIN || '.subnet.lan';
+var WEB_PORT = process.env.WEB_PORT || 3000;
+var DNS_PORT = process.env.DNS_PORT || 3053;
+
+var IPV4_PATTERN = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+var IPV6_PATTERN = /.*:.*:.*:.*:.*:.*/;
+
+var discovered = {};
+
+/* ++++++ mDNS browser ++++++ */
+
+var serviceBrowsing = [];
+var browser = mdns.browseThemAll();
+browser.on('serviceUp', function (service) {
+    if (serviceBrowsing.indexOf(service.type.name) === -1) {
+        console.log('browse service:', service.type.protocol, service.type.name);
+
+        serviceBrowsing.push(service.type.name);
+        var subBrowser = mdns.createBrowser(mdns[service.type.protocol](service.type.name));
+        subBrowser.on('error', handleError);
+        subBrowser.on('serviceUp', register);
+        subBrowser.start();
+    }
+});
+
+// var browser = mdns.createBrowser(mdns.tcp('ssh'));
+// browser.on('serviceUp', register);
+
+browser.on('serviceDown', unregister);
+browser.on('error', handleError);
+
+browser.start();
+
+function register (service) {
+    var name = service.host,
+        addresses = service.addresses;
+
+    if (name.indexOf(MDNS_DOMAIN, name.length - MDNS_DOMAIN.length) !== -1) {
+        // if name is suffixed with MDNS_DOMAIN remove it
+        name = name.substring(0, name.length - MDNS_DOMAIN.length);
+    }
+
+    name = name + BIND_DNS_DOMAIN;
+
+    console.log("service up: ", name, addresses);
+    // console.log(service);
+    discovered[name] = {};
+    addresses.forEach(function (addr) {
+        if (IPV4_PATTERN.test(addr)) {
+            discovered[name].A = addr;
+        }
+        if (IPV6_PATTERN.test(addr)) {
+            discovered[name].AAAA = addr;
+        }
+    });
+}
+
+function unregister (service) {
+    var name = service.name;
+
+    console.log("service down: ", service);
+    delete discovered[name];
+}
+
+function handleError (err) {
+  console.error(err.stack);
+}
+
+/* ++++++ DNS server ++++++ */
+
+var server = dns.createServer();
+
+server.on('request', function (request, response) {
+    // console.log(request);
+    request.question.forEach(function (q) {
+        var name = q.name;
+        console.log('DNS request for ', name);
+
+        if (name in discovered) {
+            var addr = discovered[name];
+
+            if (addr.A) {
+                response.answer.push(dns.A({
+                    name: name,
+                    address: addr.A,
+                    ttl: 600,
+                }));
+            }
+            if (addr.AAAA) {
+                response.answer.push(dns.AAAA({
+                    name: name,
+                    address: addr.AAAA,
+                    ttl: 600,
+                }));
+            }
+        }
+    });
+  response.send();
+});
+
+server.on('error', function (err, buff, req, res) {
+  console.error(err.stack);
+});
+
+server.on('socketError', function (err, buff, req, res) {
+  console.error('ERROR: Unable to dns bind server socket');
+  console.error(err.stack);
+});
+
+server.serve(DNS_PORT);
+
+/* ++++++ WEB ++++++*/
+
+var app = express();
+
+app.get('/', function (req, res) {
+    var addressesHtml = [];
+    Object.keys(discovered).sort().forEach(function (name) {
+        var addresses = discovered[name];
+        var addressHtml = [
+            '<p>',
+            util.format('<strong><a href="ssh://%s">%s</a></strong> ', name, name),
+            '(' + (addresses.A || 'no ipV4') + ' - ' + (addresses.AAAA || 'no ipV6') + ')',
+            '</p>',
+            '<code><pre>$ ' + util.format('ssh %s', name) + '</pre></code>',
+        ];
+        addressesHtml.push(addressHtml.join('\n'));
+    });
+
+    res.send([
+        '<html>',
+        '<head>',
+        '<title>mDNS Recorder</title>',
+        '</head>',
+        '<body>',
+        '<h1>mDNS Recorder</h1>',
+        addressesHtml.join('\n'),
+        '</body>',
+        '</html>'
+    ].join('\n'));
+    res.end();
+});
+
+var server = app.listen(WEB_PORT, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
